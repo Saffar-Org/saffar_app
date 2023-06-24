904a1ed3b362268c6f3e3af838d07a06144fbcb0
droy312 <dhritiman.roy312@gmail.com>
2023-06-25 00:41:28 +0530

    feat: Added font and text theme.

--- lib/main.dart
+++ lib/main.dart
@@ -14,7 +14,74 @@ class MyApp extends StatelessWidget {
       title: 'Saffar app',
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
+        useMaterial3: true,
         colorScheme: Palette.lightColorScheme,
+        fontFamily: 'Nunito',
+        textTheme: const TextTheme(
+          headline1: TextStyle(
+            fontSize: 96,
+            fontWeight: FontWeight.w300,
+            letterSpacing: -1.5,
+          ),
+          headline2: TextStyle(
+            fontSize: 60,
+            fontWeight: FontWeight.w300,
+            letterSpacing: -0.5,
+          ),
+          headline3: TextStyle(
+            fontSize: 48,
+            fontWeight: FontWeight.w400,
+          ),
+          headline4: TextStyle(
+            fontSize: 34,
+            fontWeight: FontWeight.w400,
+            letterSpacing: 0.25,
+          ),
+          headline5: TextStyle(
+            fontSize: 24,
+            fontWeight: FontWeight.w400,
+          ),
+          headline6: TextStyle(
+            fontSize: 20,
+            fontWeight: FontWeight.w500,
+            letterSpacing: 0.15,
+          ),
+          subtitle1: TextStyle(
+            fontSize: 16,
+            fontWeight: FontWeight.w400,
+            letterSpacing: 0.15,
+          ),
+          subtitle2: TextStyle(
+            fontSize: 14,
+            fontWeight: FontWeight.w500,
+            letterSpacing: 0.1,
+          ),
+          bodyText1: TextStyle(
+            fontSize: 16,
+            fontWeight: FontWeight.w400,
+            letterSpacing: 0.5,
+          ),
+          bodyText2: TextStyle(
+            fontSize: 14,
+            fontWeight: FontWeight.w400,
+            letterSpacing: 0.25,
+          ),
+          button: TextStyle(
+            fontSize: 14,
+            fontWeight: FontWeight.w500,
+            letterSpacing: 1.25,
+          ),
+          caption: TextStyle(
+            fontSize: 12,
+            fontWeight: FontWeight.w400,
+            letterSpacing: 0.4,
+          ),
+          overline: TextStyle(
+            fontSize: 10,
+            fontWeight: FontWeight.w400,
+            letterSpacing: 1.5,
+          ),
+        ),
       ),
       home: const MyHomePage(title: 'Flutter Demo Home Page'),
     );
