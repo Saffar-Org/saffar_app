cb4ce0035e22a87e8a49804a247de1bfb3ecd75d
droy312 <dhritiman.roy312@gmail.com>
2023-07-04 16:04:06 +0530

    chor: Store tomtom map api key in .env file

--- lib/core/constants/strings.dart
+++ lib/core/constants/strings.dart
@@ -2,5 +2,6 @@ import 'package:flutter_dotenv/flutter_dotenv.dart';
 
 class Strings {
   static String baseApiUrl = dotenv.env['BASE_API_URL']!;
-  static String saffarCaption = 'Ride with ease and comfort.';
+  static String mapApiKey = dotenv.env['TOMTOM_MAP_API']!;
+  static const String saffarCaption = 'Ride with ease and comfort.';
 }
--- lib/features/view_map/presenter/screens/view_map_screen.dart
+++ lib/features/view_map/presenter/screens/view_map_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_map/flutter_map.dart';
 import 'package:latlong2/latlong.dart';
+import 'package:saffar_app/core/constants/strings.dart';
 
 class ViewMapScreen extends StatefulWidget {
   const ViewMapScreen({Key? key}) : super(key: key);
@@ -13,7 +14,6 @@ class ViewMapScreen extends StatefulWidget {
 
 class _ViewMapScreenState extends State<ViewMapScreen> {
   final LatLng _tomTomHq = LatLng(32.43, 74.54);
-  final String _mapApiKey = "eipsnjcjgkiMWB8WNCp9Rq0pFWayp3AK";
 
   @override
   Widget build(BuildContext context) {
@@ -27,7 +27,7 @@ class _ViewMapScreenState extends State<ViewMapScreen> {
         layers: [
           TileLayerOptions(
             urlTemplate:
-                "https://api.tomtom.com/map/1/tile/basic/night/{z}/{x}/{y}.png?key=$_mapApiKey",
+                "https://api.tomtom.com/map/1/tile/basic/night/{z}/{x}/{y}.png?key=${Strings.mapApiKey}",
           ),
           MarkerLayerOptions(
             markers: [
