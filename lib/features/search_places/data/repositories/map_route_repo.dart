cdd0514bcd01d2ca3f22fd723144130355307143
droy312 <dhritiman.roy312@gmail.com>
2023-07-09 16:57:38 +0530

    feat: Add map route repo and get points from source to destination usecase

---
+++ lib/features/search_places/data/repositories/map_route_repo.dart
@@ -0,0 +1,42 @@
+import 'package:dio/dio.dart';
+import 'package:latlong2/latlong.dart';
+import 'package:saffar_app/core/errors/custom_exception.dart';
+import 'package:saffar_app/core/service_locator.dart';
+
+import '../../../../core/constants/strings.dart';
+
+class MapRouteRepo {
+  final Dio _dio = sl<Dio>();
+
+  /// Gets the routes points from source to destination
+  Future<List<LatLng>> getPointsFromSourceToDestination(
+    LatLng sourceLatLng,
+    LatLng destinationLatLng,
+  ) async {
+    try {
+      final Response response = await _dio.get(
+        'https://api.tomtom.com/routing/1/calculateRoute/${sourceLatLng.latitude},${sourceLatLng.longitude}:${destinationLatLng.latitude},${destinationLatLng.longitude}/json?key=${Strings.mapApiKey}',
+      );
+
+      final List list = response.data['routes']['legs']['points'] as List;
+
+      final List<Map<String, dynamic>> maps =
+          list.map((e) => e as Map<String, dynamic>).toList();
+
+      final List<LatLng> points = [];
+
+      for (final map in maps) {
+        final double? lat = double.tryParse(map['latitude']);
+        final double? lon = double.tryParse(map['longitude']);
+
+        if (lat != null && lon != null) {
+          points.add(LatLng(lat, lon));
+        }
+      }
+
+      return points;
+    } catch (e) {
+      throw CustomException(message: e.toString());
+    }
+  }
+}
---
+++ lib/features/search_places/domain/usecases/get_points_from_source_to_destination_usecase.dart
@@ -0,0 +1,28 @@
+import 'package:dartz/dartz.dart';
+import 'package:saffar_app/core/errors/custom_exception.dart';
+import 'package:saffar_app/core/errors/failure.dart';
+import 'package:latlong2/latlong.dart';
+import 'package:saffar_app/core/service_locator.dart';
+import 'package:saffar_app/features/search_places/data/repositories/map_route_repo.dart';
+
+class GetPointsFromSourceToDestinationUsecase {
+  final MapRouteRepo _mapRoutesRepo = sl<MapRouteRepo>();
+
+  /// Gest the routes points from source to destination
+  Future<Either<Failure, List<LatLng>>> call(
+    LatLng sourceLatLng,
+    LatLng destinationLatLng,
+  ) async {
+    try {
+      final List<LatLng> points =
+          await _mapRoutesRepo.getPointsFromSourceToDestination(
+        sourceLatLng,
+        destinationLatLng,
+      );
+
+      return Right(points);
+    } on CustomException catch (e) {
+      return Left(Failure.fromCustomException(e));
+    }
+  }
+}
